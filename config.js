dc8e05fe49c975ccd793e57cdd48592587c12292
jin1963 <kaojino89@gmail.com>
2025-07-22 10:39:51 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// ✅ ใช้ค่าจากระบบจริงของคุณ
+const contractAddress = "0x59e40eA79DA2B59A6f4C5D2Cb2bB162E48db2aE2"; // KJCStaked
+const tokenAddress = "0xd479ae350dc24168e8db863c5413c35fb2044ecd";   // KJC Token
+const chainId = 56; // BNB Smart Chain Mainnet
